a3cd6be295a5d06b1605144ea7c32bee5344cd93
Parker <pp101socials@gmail.com>
2023-09-14 08:02:11 -0400

    Fixed number issue?

--- pages/index.js
+++ pages/index.js
@@ -13,49 +13,26 @@ import "../assets/styles/modules/Index/Index.module.css";
 
 export default function Home() {
   const router = useRouter();
-  const [totalUniqueIPs, setTotalUniqueIPs] = useState(null);
+  const [TOTAL_UNIQUE_IPS, SET_TOTAL_UNIQUE_IPS] = useState(null);
 
   useEffect(() => {
-    // Function to fetch data from the API route
-    const fetchTotalIPCount = async () => {
-      try {
-        const response = await fetch("/api/trackIps");
-        if (response.ok) {
-          const data = await response.json();
-          const totalIPs = data.TOTAL_UNIQUE_IPS;
-
-          // Store the total IP count in localStorage
-          localStorage.setItem("totalIPs", totalIPs);
-
-          // Set the state with the total IP count
-          setTotalUniqueIPs(totalIPs);
-        } else {
-          console.error("Network response was not ok");
-        }
-      } catch (error) {
-        console.error("Error:", error);
-      }
-    };
-
-    // Check if the total IP count is already in localStorage
-    const storedTotalIPs = localStorage.getItem("totalIPs");
-    if (storedTotalIPs !== null) {
-      // Use the count from localStorage
-      setTotalUniqueIPs(parseInt(storedTotalIPs, 10));
-    } else {
-      // Fetch and store the count if not found in localStorage
-      fetchTotalIPCount();
-    }
+    // Fetching the api route data
+    fetch("/api/trackIps")
+      .then((response) => response.json())
+      .then((data) => {
+        SET_TOTAL_UNIQUE_IPS(data.TOTAL_UNIQUE_IPS);
+      })
+      .catch((error) => {
+        console.error("Error: " + error);
+      });
   }, []);
 
   return (
     <div>
       <h1>Total Number of Visits</h1>
 
-      {totalUniqueIPs !== null ? (
-        <p>
-          You have <strong>{totalUniqueIPs}</strong> visits on this website!.
-        </p>
+      {TOTAL_UNIQUE_IPS !== null ? (
+        <p>Total: {TOTAL_UNIQUE_IPS} visits.</p>
       ) : (
         <p>Loading ...</p>
       )}
