48b692fd7f947350a9f342836e7a7d1c5c30d7c9
Parker <pp101socials@gmail.com>
2023-09-14 09:11:44 -0400

    Added feature to remove AWS ips

--- pages/api/trackIps.js
+++ pages/api/trackIps.js
@@ -1,6 +1,20 @@
 import { connectToDatabase } from "@/utils/mongodb-connection";
+import axios from "axios";
 
 // const IPSTACK_KEY = "6eafccee3db72681fcd3589b807c0f2e";
+
+const fetchAWSIPRanges = async () => {
+  try {
+    const response = await axios.get(
+      "https://ip-ranges.amazonaws.com/ip-ranges.json"
+    );
+    const data = response.data;
+    return data.prefixes; // This contains the AWS IP address ranges
+  } catch (error) {
+    console.error("Error fetching AWS IP ranges:", error);
+    return [];
+  }
+};
 
 export default async (req, res) => {
   const { headers } = req;
@@ -48,7 +62,8 @@ export default async (req, res) => {
     }
 
     // Removing null, Amazon Web Services (AWS) ip addresses
-    await IP_COLLECTION.deleteOne({ IP_ADDRESS: "3.238.66.225" });
+    const AWS_IP_RANGES = await fetchAWSIPRanges();
+    await IP_COLLECTION.deleteMany({ IP_ADDRESS: { $in: AWS_IP_RANGES } });
     await IP_COLLECTION.deleteMany({ IP_ADDRESS: null });
 
     // Removing 3.238.66.225 ip (I have no idea what this is. I assume it might be Netlify)
