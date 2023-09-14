5a6800f6e439b18a5855950afea2ee7c3f69c9ac
Parker <pp101socials@gmail.com>
2023-09-14 07:56:05 -0400

    Added assetPrefix url

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const isProd = process.env.NODE_ENV === "production";
+const fs = require("fs-extra");
 
 module.exports = {
   reactStrictMode: true,
@@ -14,5 +15,21 @@ module.exports = {
   //! This added clean urls and eliminates the .html extension aswell as linking
   //! CSS and JS with the trailingSlash
   assetPrefix: isProd ? "https://melodious-bunny-897f80.netlify.app/" : "",
+  // assetPrefix: () => {
+  //   if (isProd) {
+  //     if (process.env.DOMAIN === "codingthefront.com") {
+  //       return "https://codingthefront.com/";
+  //     } else {
+  //       return "https://glittering-cupcake-b97603.netlify.app/";
+  //     }
+  //   } else {
+  //     return "";
+  //   }
+  // },
+
   trailingSlash: true,
+
+  async afterBuild({ utils }) {
+    await fs.copy("_headers", ".next/_headers");
+  },
 };
